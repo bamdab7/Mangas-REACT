1a45b442c72d12635f6ccf96ac8b4dab5c85ce89
nekoo <nereapena42gmail.com>
2022-03-08 22:29:05 +0100

    mas cambios en el footer

--- reactmanganpc/src/components/Footer/Footer.js
+++ reactmanganpc/src/components/Footer/Footer.js
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import './Footer.css';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 const Footer =()=>{
 
     return(
-
+    
     <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <h1>texto</h1>
+    <div className="footer">    
+        <Grid container spacing={0.5}>
+            <Grid item xs={6}>
+                <p className="textoFooter">Lorem ipsum dolor sit amet consectetur.</p> 
+                <p className="textoFooter">Accumsan neque molestie luctus pellentesque blandit justo sollicitudis.</p>
+                <p className="textoFooter">Dis lobortis orci porttitor malesuada eget hac tellus aenean montes massa metus non.</p>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={6}>
                 <h1>texto</h1>
             </Grid>
             <Grid item xs={4}>
@@ -23,6 +25,7 @@ const Footer =()=>{
                 <h1>texto</h1>
             </Grid>
         </Grid>
+    </div>    
     </Box>
     
     );

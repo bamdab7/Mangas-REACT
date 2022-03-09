d7babc301015c2804c8957e388667020b6a4ef84
nekoo <nereapena42gmail.com>
2022-03-09 18:39:12 +0100

    fin footer

--- reactmanganpc/src/components/Footer/Footer.js
+++ reactmanganpc/src/components/Footer/Footer.js
@@ -2,6 +2,13 @@ import * as React from 'react';
 import './Footer.css';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import EmailIcon from '@mui/icons-material/Email';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PaymentIcon from '@mui/icons-material/Payment';
+import ContactlessIcon from '@mui/icons-material/Contactless';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 
 const Footer =()=>{
 
@@ -15,14 +22,36 @@ const Footer =()=>{
                 <p className="textoFooter">Accumsan neque molestie luctus pellentesque blandit justo sollicitudis.</p>
                 <p className="textoFooter">Dis lobortis orci porttitor malesuada eget hac tellus aenean montes massa metus non.</p>
             </Grid>
-            <Grid item xs={6}>
-                <h1>texto</h1>
+            <Grid className="producto" item xs={6}>
+                <Box sx={{ width: '50%' }}>
+                    <Grid container spacing={1} rowSpacing={1.5}>
+                        <Grid item xs={3}>
+                            <EmailIcon/>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <FacebookIcon/>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <InstagramIcon/>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <TwitterIcon/>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <PaymentIcon/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <ContactlessIcon/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <CardGiftcardIcon/>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Grid>
-            <Grid item xs={4}>
-                <h1>texto</h1>
-            </Grid>
-            <Grid item xs={8}>
-                <h1>texto</h1>
+            <Grid className="footertexto" item xs={12}>
+                <p>Venenatis ultrices himenaeos sollicitudin et eros habitant magna felis</p>
             </Grid>
         </Grid>
     </div>    
